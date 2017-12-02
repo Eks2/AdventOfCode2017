c4d09ac02c3af558dee97257f28e9723648d766d
Ekin Inceleme <einceleme@gmail.com>
2017-12-02 10:57:11 +0000

    Day 2 complete!

---
+++ day2/problem.py
@@ -0,0 +1,32 @@
+import csv
+
+
+def calculate_checksum(file_path):
+    checksum = 0
+    with open(file_path, 'r') as checksum_file:
+        checksum_reader = csv.reader(checksum_file.readlines(), delimiter='\t')
+
+        for row in checksum_reader:
+            int_list = [int(i) for i in row]
+            checksum += max(int_list) - min(int_list)
+    return checksum
+
+
+def calculate_evenly_divisible_checksum(file_path):
+    checksum = 0
+    with open(file_path, 'r') as checksum_file:
+        checksum_reader = csv.reader(checksum_file.readlines(), delimiter='\t')
+
+        for row in checksum_reader:
+            int_list = [int(i) for i in row]
+
+            for i in int_list:
+                for j in int_list:
+                    calc = float(i)/j
+                    if calc.is_integer()and calc != 1:
+                        checksum += calc
+                        break
+
+    return int(checksum)
+
+
---
+++ test/day2/test_solution.py
@@ -0,0 +1,24 @@
+import unittest
+from mock import mock_open, patch
+from day2.problem import *
+
+
+class TestChecksumCalculator(unittest.TestCase):
+
+    @patch('day2.problem.open', mock_open(read_data='5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8'), create=True)
+    def test_checksum_example_1(self):
+        result = calculate_checksum('this_mock_example')
+        self.assertEqual(18, result)
+
+    def test_real_checksum(self):
+        result = calculate_checksum('input.txt')
+        self.assertEqual(41887, result)
+
+    @patch('day2.problem.open', mock_open(read_data='5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5'), create=True)
+    def test_evenly_divisible_checksum_example_1(self):
+        result = calculate_evenly_divisible_checksum('this_mock_example')
+        self.assertEqual(9, result)
+
+    def test_real_evenly_divisible_checksum_example_1(self):
+        result = calculate_evenly_divisible_checksum('input.txt')
+        print result
